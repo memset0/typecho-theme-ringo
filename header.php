2f388aa5cfc4e28e1a41b46e0ee207de29edacf5
abc1763613206 <abc1763613206@163.com>
2019-06-15 11:03:30 +0800

    fix:

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="renderer" content="webkit">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <I-- css3-mediaqueries-js 用于尝试在上古浏览器支持自适应viewport函数 -->
+    <!-- css3-mediaqueries-js 用于尝试在上古浏览器支持自适应viewport函数 -->
     <!--[if lt IE 9]>
     　　<script src="https://cdn.jsdelivr.net/gh/livingston/css3-mediaqueries-js@master/css3-mediaqueries.min.js"></script>
     <![endif]-->
